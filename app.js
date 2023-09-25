a69802c85c1384e5ed5e9ec8c01f337c1d2cc020
EirBec <eirbec.kodehode@gmail.com>
2023-09-25 09:22:45 +0200

    fixed bug on page when clicking the first top lef button

--- app.js
+++ app.js
@@ -3,174 +3,79 @@ let marsQuake = new Audio("Marsquake.mp3");
 let soundsOfSaturn = new Audio("Sounds of Saturn.mp3");
 let jupitersAuroras = new Audio("Jupiter's Auroras.mp3");
 
-let roarJupiter = document.querySelector("#roarOfJupiter");
-let mars = document.querySelector("#marsQuake");
-let saturn = document.querySelector("#soundsOfSaturn");
-let jupiterAuroras = document.querySelector("#jupitersAuroras");
-
-
-
-
-
+let roarJupiterButton = document.querySelector("#roarOfJupiter");
+let marsButton = document.querySelector("#marsQuake");
+let saturnButton = document.querySelector("#soundsOfSaturn");
+let jupiterAurorasButton = document.querySelector("#jupitersAuroras");
 
 function playJupiter() {
   if (roarOfJupiter.paused) {
     roarOfJupiter.play();
-    roarJupiter.textContent = "Pause Audio (Q)";
+    roarJupiterButton.textContent = "Pause Audio (Q)";
   } else {
     roarOfJupiter.pause();
-    roarJupiter.textContent = "Roar of Jupiter (Q)";
+    roarJupiterButton.textContent = "Roar of Jupiter (Q)";
   }
 }
 
-roarJupiter.addEventListener("click", playJupiter);
+roarJupiterButton.addEventListener("click", playJupiter);
 
 window.addEventListener("keypress", (event) => {
-  if (event.key === "q") {
-    playJupiter();
-  } else if (event.key === "Q") {
+  if (event.key.toLowerCase() === "q") {
     playJupiter();
   }
 });
-
-
-
-
-
 
 function playMars() {
-    if (marsQuake.paused) {
-      marsQuake.play();
-      mars.textContent = "Pause Audio (W)";
-    } else {
-      marsQuake.pause();
-      mars.textContent = "Marsquake (W)";
-    }
+  if (marsQuake.paused) {
+    marsQuake.play();
+    marsButton.textContent = "Pause Audio (W)";
+  } else {
+    marsQuake.pause();
+    marsButton.textContent = "Marsquake (W)";
+  }
 }
 
-roarJupiter.addEventListener("click", playMars);
+marsButton.addEventListener("click", playMars);
 
 window.addEventListener("keypress", (event) => {
-  if (event.key === "w") {
-    playMars();
-  } else if (event.key === "W") {
+  if (event.key.toLowerCase() === "w") {
     playMars();
   }
 });
-
-
-
-
 
 function playSaturn() {
   if (soundsOfSaturn.paused) {
     soundsOfSaturn.play();
-    saturn.textContent = "Pause Audio (E)";
+    saturnButton.textContent = "Pause Audio (E)";
   } else {
     soundsOfSaturn.pause();
-    saturn.textContent = "Sounds of Saturn (E)";
+    saturnButton.textContent = "Sounds of Saturn (E)";
   }
 }
 
-soundsOfSaturn.addEventListener("click", playSaturn);
+saturnButton.addEventListener("click", playSaturn);
 
 window.addEventListener("keypress", (event) => {
-  if (event.key === "e") {
-    playSaturn();
-  } else if (event.key === "E") {
+  if (event.key.toLowerCase() === "e") {
     playSaturn();
   }
 });
-
-
-
-
 
 function playJupiterA() {
   if (jupitersAuroras.paused) {
     jupitersAuroras.play();
-    jupiterAuroras.textContent = "Pause Audio (R)";
+    jupiterAurorasButton.textContent = "Pause Audio (R)";
   } else {
     jupitersAuroras.pause();
-    jupiterAuroras.textContent = "Jupiter's Auroras (R)";
+    jupiterAurorasButton.textContent = "Jupiter's Auroras (R)";
   }
 }
 
-jupitersAuroras.addEventListener("click", playJupiterA);
+jupiterAurorasButton.addEventListener("click", playJupiterA);
 
 window.addEventListener("keypress", (event) => {
-  if (event.key === "r") {
-    playJupiterA();
-  } else if (event.key === "R") {
+  if (event.key.toLowerCase() === "r") {
     playJupiterA();
   }
 });
-
-
-
-// function playSaturn() {
-//   if (roarOfJupiter.paused) {
-//     roarOfJupiter.play();
-//     roarJupiter.textContent = "Pause Audio";
-//   } else {
-//     roarOfJupiter.pause();
-//     roarJupiter.textContent = "Roar of Jupiter";
-//   }
-// }
-
-// roarJupiter.addEventListener("click", playJupiter);
-
-// window.addEventListener("keypress", (event) => {
-//   if (event.key === "q" || "Q") {
-//     playJupiter();
-//   }
-// });
-
-// function playJupiterA() {
-//   if (roarOfJupiter.paused) {
-//     roarOfJupiter.play();
-//     roarJupiter.textContent = "Pause Audio";
-//   } else {
-//     roarOfJupiter.pause();
-//     roarJupiter.textContent = "Roar of Jupiter";
-//   }
-// }
-
-// roarJupiter.addEventListener("click", playJupiter);
-
-// window.addEventListener("keypress", (event) => {
-//   if (event.key === "q" || "Q") {
-//     playJupiter();
-//   }
-// });
-
-
-// mars.addEventListener("click", () => {
-//   if (marsQuake.paused) {
-//     marsQuake.play();
-//     mars.textContent = "Pause Audio";
-//   } else {
-//     marsQuake.pause();
-//     mars.textContent = "Marsquake";
-//   }
-// });
-
-// saturn.addEventListener("click", () => {
-//   if (soundsOfSaturn.paused) {
-//     soundsOfSaturn.play();
-//     saturn.textContent = "Pause Audio";
-//   } else {
-//     soundsOfSaturn.pause();
-//     saturn.textContent = "Sounds of Saturn";
-//   }
-// });
-
-// jupiterAuroras.addEventListener("click", () => {
-//   if (jupitersAuroras.paused) {
-//     jupitersAuroras.play();
-//     jupiterAuroras.textContent = "Pause Audio";
-//   } else {
-//     jupitersAuroras.pause();
-//     jupiterAuroras.textContent = "Jupiter's Auroras";
-//   }
-// });
